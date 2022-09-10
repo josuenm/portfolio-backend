a800a9780e29cf19227995ee647e3409ebab5a06
josuenm <direct.josue@gmail.com>
2022-09-10 13:47:30 -0300

    feat: using env variables
    Using env variables in port and cors origin

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,25 @@
 import cors from "cors";
+import "dotenv/config";
 import express from "express";
 import "./databases/mongodb";
 import { router } from "./routes/index.routes";
 
-const port = 8080;
+const CORS_ORIGIN =
+  process.env.NODE_ENV === "production"
+    ? process.env.CORS_ORIGIN
+    : process.env.CORS_ORIGIN_DEVELOPMENT;
+
+const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: CORS_ORIGIN,
+  })
+);
 
 app.use("/api", router);
 
-app.listen(port, () => console.log("Server is running 🚀"));
+app.listen(PORT || 8080, () => console.log("Server is running 🚀"));
