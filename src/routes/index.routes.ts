ff5682cc283574f1b41acd871fb9b44f4050b52b
josuenm <direct.josue@gmail.com>
2022-09-10 15:57:05 -0300

    feat: new route added
    userData router added

--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,8 +1,10 @@
 import express from "express";
 import { contactRouter } from "./contact.routes";
+import { userDataRouter } from "./userData.routes";
 
 const router = express.Router();
 
 router.use("/contact", contactRouter);
+router.use("/userData", userDataRouter);
 
 export { router };
