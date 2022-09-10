dbeb549e2767858eb2fc387e72794a8e8ba9e61f
josuenm <direct.josue@gmail.com>
2022-09-10 13:46:25 -0300

    feat: mongoDB configurated

--- src/databases/mongodb.ts
+++ src/databases/mongodb.ts
@@ -1,7 +1,13 @@
+import "dotenv/config";
 import mongoose from "mongoose";
 
+const MONGO_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URL
+    : process.env.MONGO_URL_DEVELOPMENT;
+
 try {
-  mongoose.connect("mongodb://localhost:27017/portfolio");
+  mongoose.connect(`${MONGO_URL}`);
   console.log("Connected to MongoDB");
 } catch (err) {
   console.error(err);
