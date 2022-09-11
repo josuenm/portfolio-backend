63e907c0448752efead963d2aeae2fcbcf6b5f39
josuenm <direct.josue@gmail.com>
2022-09-11 17:21:32 -0300

    feat: tracking removed

--- src/modules/userData/repositories/implementations/IUserDataRepository.ts
+++
@@ -1,5 +0,0 @@
-import { IUserData } from "../../models/IUserData";
-
-export interface IUserDataRepository {
-  create: (contact: IUserData) => Promise<string>;
-}
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -1,10 +1,8 @@
 import express from "express";
 import { contactRouter } from "./contact.routes";
-import { userDataRouter } from "./userData.routes";
 
 const router = express.Router();
 
 router.use("/contact", contactRouter);
-router.use("/userData", userDataRouter);
 
 export { router };
