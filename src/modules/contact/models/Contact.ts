76605e5edf844d0875b353c48f0c1b11ccaf23e7
josuenm <direct.josue@gmail.com>
2022-09-15 01:51:10 -0300

    feat: timestamp added in model

--- src/modules/contact/models/Contact.ts
+++ src/modules/contact/models/Contact.ts
@@ -1,11 +1,14 @@
 import mongoose from "mongoose";
 
-const schema = new mongoose.Schema({
-  id: { type: "string", unique: true },
-  name: { type: "string" },
-  email: { type: "string" },
-  phoneNumber: { type: "string" },
-});
+const schema = new mongoose.Schema(
+  {
+    id: { type: "string", unique: true },
+    name: { type: "string" },
+    email: { type: "string" },
+    phoneNumber: { type: "string" },
+  },
+  { timestamps: true }
+);
 
 const Contact = mongoose.model("Contact", schema);
 
